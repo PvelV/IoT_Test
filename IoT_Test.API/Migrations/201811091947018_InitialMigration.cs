f5aac415145f54d7d9724a6cfa7f718b662c09a7
Pavel Vaverka <pvel.vaverka@centrum.cz>
2018-11-09 20:47:20 +0100

    Initial DB migration

---
+++ IoT_Test.API/Migrations/201811091947018_InitialMigration.cs
@@ -0,0 +1,27 @@
+namespace IoT_Test.API.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Messages",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        DateTime = c.DateTime(nullable: false),
+                        Value = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Messages");
+        }
+    }
+}
